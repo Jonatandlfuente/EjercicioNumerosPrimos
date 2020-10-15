82141a66b2509f6ff48fcf88b5a3dca6805d6e5f
Jonatandlfuente <60443605+Jonatandlfuente@users.noreply.github.com>
2020-10-15 13:47:25 +0200

    Add files via upload

---
+++ src/models/Primo.java
@@ -0,0 +1,56 @@
+package models;
+
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Primo extends Thread {
+
+	private int totalDePrimos;
+	private String nombre;
+	private int rangoInicial;
+	private int rangoFinal;
+
+	// Constructor con los rangos que recorrerá la lógica del hilo
+	public Primo(int rangoInicial, int rangoFinal) {
+		super();
+		this.rangoInicial = rangoInicial;
+		this.rangoFinal = rangoFinal;
+	}
+
+	public int getTotalDePrimos() {
+		return totalDePrimos;
+	}
+
+	public void setTotalDePrimos(int totalDePrimos) {
+		this.totalDePrimos = totalDePrimos;
+	}
+
+	@Override
+	public void run() {
+		int aux = 0;
+		// la lógica del hilo nos verifica si un número es primo entre dos rangos
+		for (int i = rangoInicial; i <= rangoFinal; i++) {
+			verificarPrimo(i);
+		}
+	}
+
+	/*
+	 * Método que verifica si un número es primo y si lo es lo muestra por pantalla,
+	 * devuelve un booleano que utilizaremos para contar el número de primos
+	 */
+	public void verificarPrimo(int numeroPrimo) {
+		boolean primo = false;
+
+		BigInteger bigInteger = new BigInteger(String.valueOf(numeroPrimo));
+		primo = bigInteger.isProbablePrime(numeroPrimo);
+		if (primo != false) {
+			/*
+			 * Prueba para contar el número de primos, no funciona
+			 * Primo.setTotalDePrimos(Primo.getTotalDePrimos()+1)
+			 */
+			this.totalDePrimos++;
+			System.out.println(numeroPrimo);
+		}
+	}
+}
